79c7bcda7426d22f8ab3ebc6537af683e1a0e0b2
EduardoAcosta19 <l161130055@gamadero.tecnm.mx>
2021-01-15 00:06:07 -0600

    estilos funcionando

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-console */
 // Script de Frontend
+// Importar los estilos
+import './styles/styles.css';
+
 console.log('Modern JS working!!! Thanks Babel...');
-const show = (message = 'saludo por defecto') => {
-  alert(message);
-};
-// Ejemplo
-show();
-show('Mi saludo');
+// const show = (message = 'saludo por defecto') => {
+//  alert(message);
+// };
