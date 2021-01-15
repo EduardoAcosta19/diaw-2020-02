1d1cccbd603a590d1df05ebafd9afbfecaaf736e
EduardoAcosta19 <l161130055@gamadero.tecnm.mx>
2021-01-15 00:47:28 -0600

    Materialize funcionando

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 /* eslint-disable no-console */
 // Script de Frontend
 // Importar los estilos
-import './styles/styles.css';
+// import './styles/styles.css';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize';
 
 console.log('Modern JS working!!! Thanks Babel...');
 // const show = (message = 'saludo por defecto') => {
