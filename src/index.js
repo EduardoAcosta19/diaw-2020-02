146b85650e6d72b97ad02440cd2d54f072f30184
EduardoAcosta19 <l161130055@gamadero.tecnm.mx>
2021-01-19 07:47:28 -0600

    Materialize Working testing With Colors

--- src/index.js
+++ src/index.js
@@ -5,8 +5,3 @@
 // import './styles/styles.css';
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize';
-
-console.log('Modern JS working!!! Thanks Babel...');
-// const show = (message = 'saludo por defecto') => {
-//  alert(message);
-// };
